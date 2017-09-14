ae467a4a8e7f3ddc206b08702df4743e7e5fd125
Dre mf Randaci <drerandaci@gmail.com>
2017-09-14 10:18:40 -0500

    fixed deselecting functionality

--- javascripts/domHandler.js
+++ javascripts/domHandler.js
@@ -6,11 +6,41 @@ let bread = document.getElementById("bread");
 let veggies = document.getElementById("veggies");
 let btn = document.getElementById("btn");
 
-meat.addEventListener('change', meatBuilder);
-condiments.addEventListener('change', condimentBuilder);
-cheese.addEventListener('change', cheeseBuilder);
-bread.addEventListener('change', breadBuilder);
-veggies.addEventListener('change', veggieBuilder);
+meat.addEventListener('change', (e) => {
+	let index = allCheckedItems.indexOf(checkedMeat)
+    if (index > -1){
+      allCheckedItems.splice(index, 1);
+    }
+	meatBuilder(e)
+});
+condiments.addEventListener('change', (e) => {
+	let index = allCheckedItems.indexOf(checkedConds)
+    if (index > -1){
+      allCheckedItems.splice(index, 1);
+    }
+	condimentBuilder(e)
+});
+cheese.addEventListener('change', (e) => {
+	let index = allCheckedItems.indexOf(checkedCheese)
+    if (index > -1){
+      allCheckedItems.splice(index, 1);
+    }
+	cheeseBuilder(e)
+});
+bread.addEventListener('change', (e) => {
+	let index = allCheckedItems.indexOf(checkedBread)
+    if (index > -1){
+      allCheckedItems.splice(index, 1);
+    }
+    breadBuilder(e);
+});
+veggies.addEventListener('change', (e) => {
+	let index = allCheckedItems.indexOf(checkedVeggie)
+    if (index > -1){
+      allCheckedItems.splice(index, 1);
+    }
+    veggieBuilder(e);
+});
 btn.addEventListener('click', (e) => {
 	clearDom();
 	writeToDom(sammieAndBreadCombinedPrice);
